9b4d869b42cbe31c301c60e30e403a971e699874
ionutms <mihai.ionut.career@gmail.com>
2025-06-13 21:19:18 +0300

    Refactor display statements in design and testbench modules
    - Updated design_under_test.sv to include an initial empty display line before the message.
    - Modified design_under_test_testbench.sv to add an initial empty display line after the testbench message.
    - Adjusted comments for clarity and consistency.
    - Incremented execution counts in the notebook to reflect the changes.

--- notebooks/Chapter_2_examples/example_1/design_under_test.sv
+++ notebooks/Chapter_2_examples/example_1/design_under_test.sv
@@ -1,4 +1,5 @@
 // design_under_test.sv
-module design_module_name ();  // Design under test
-  initial $display("Hello from design!");  // Display message
+module design_module_name ();               // Design under test
+  initial $display();                       // Display empty line
+  initial $display("Hello from design!");   // Display message
 endmodule
--- notebooks/Chapter_2_examples/example_1/design_under_test_testbench.sv
+++ notebooks/Chapter_2_examples/example_1/design_under_test_testbench.sv
@@ -4,10 +4,11 @@ module test_bench_module;  // Testbench module
 
   initial begin
     // Dump waves
-    $dumpfile("test_bench_module.vcd");  // Specify the VCD file
-    $dumpvars(0, test_bench_module);     // Dump all variables in the test module
-    #1;                                 // Wait for a time unit
-    $display("Hello from testbench!");  // Display message
+    $dumpfile("test_bench_module.vcd");       // Specify the VCD file
+    $dumpvars(0, test_bench_module);          // Dump all variables in the test module
+    #1;                                       // Wait for a time unit
+    $display("Hello from testbench!");        // Display message
+    $display();                               // Display empty line
 
   end
 
